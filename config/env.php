1981f63aed68c5f8870fefbe9c0787637d65e020
Gabrielmoreno1596 <127924898+Gabrielmoreno1596@users.noreply.github.com>
2025-11-12 00:23:06 -0600

    Uncomment database configuration settings
    Para hosting - production

--- config/env.php
+++ config/env.php
@@ -7,14 +7,14 @@ function env(string $key, ?string $default = null): ?string
 }
 
 // para el hosting
-/* define('DB_DSN',  'mysql:host=localhost;dbname=cecnsr_prod;charset=utf8mb4');
+define('DB_DSN',  'mysql:host=localhost;dbname=cecnsr_prod;charset=utf8mb4');
 define('DB_USER', 'cecnsr_app');
 define('DB_PASS', 'PON_AQUI_TU_PASSWORD_FUERTE');
-define('APP_KEY', 'PEGA_AQUI_un_bin2hex(random_bytes(32))'); */ // genera en tu PC o en PHP CLI
+define('APP_KEY', 'PEGA_AQUI_un_bin2hex(random_bytes(32))'); // genera en tu PC o en PHP CLI
 
 // para el localhost
 
-define('DB_DSN', env('DB_DSN', 'mysql:host=127.0.0.1;dbname=cecnsr;charset=utf8mb4'));
+/* define('DB_DSN', env('DB_DSN', 'mysql:host=127.0.0.1;dbname=cecnsr;charset=utf8mb4'));
 define('DB_USER', env('DB_USER', 'root'));
 define('DB_PASS', env('DB_PASS', ''));
-define('APP_KEY', env('APP_KEY', 'please-generate-a-secure-random-key'));
+define('APP_KEY', env('APP_KEY', 'please-generate-a-secure-random-key')); */
